9e02592a7ae82e8a2b03ad6e970fa17665378fa8
Angel Giovanny Vidal Castillo <vidalcastillo.agvc@gmail.com>
2022-06-14 17:17:18 -0600

    Actividad_10

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'gio'},
+          {key: 'angel'},
+          {key: 'paty'},
+          {key: 'mary'},
+          {key: 'almeida'},
+          
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+}
+
+export default FlatListBasics;
